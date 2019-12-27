865f9e843068c8fe64b0bdd9850b944ae29e0984
1216339373 <1216339373@qq.com>
2019-12-27 23:34:37 +0800

    衡山城demo项目搭建框架

---
+++ hengshancheng/src/main/java/com/konglei/HengShanCheng.java
@@ -0,0 +1,13 @@
+package com.konglei;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HengShanCheng {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HengShanCheng.class, args);
+    }
+}
---
+++ hengshancheng/src/main/java/com/konglei/repair/RepairController.java
@@ -0,0 +1,4 @@
+package com.konglei.repair;
+
+public class RepairController {
+}
